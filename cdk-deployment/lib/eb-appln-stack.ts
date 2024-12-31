20ce834100bc7b91439ad95d4c0851349c388cea
King <AKing@elementcorp.com>
2024-12-30 21:36:40 -0500

    silly change to commit

--- cdk-deployment/lib/eb-appln-stack.ts
+++ cdk-deployment/lib/eb-appln-stack.ts
@@ -6,6 +6,7 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 console.log(__dirname);
+console.log(__dirname);
 
 export interface EBEnvProps extends cdk.StackProps {
   // Autoscaling group configuration
